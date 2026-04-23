2375ec75842c13a082f93b5a97b0c385c8d5d8ab
yona hani <yonahani@MacBook-Pro-yona.local>
2026-04-23 15:19:36 +0200

    yaqeen hani task

--- include/user.h
+++ include/user.h
@@ -1,1 +1,19 @@
+#ifndef USER_H
+#define USER_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class User {
+private:
+    int id;
+    string name;
+    string role;
+
+public:
+    User(int i, string n, string r);
+    string getRole();
+};
+
+#endif
--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,11 @@
+#include "../include/user.h"
 
+User::User(int i, string n, string r) {
+    id = i;
+    name = n;
+    role = r;
+}
+
+string User::getRole() {
+    return role;
+}
